7801d1be06d3ebc7229ec9cc05b49bb20ce85610
Alexander Kubel <101325014+Alecsen@users.noreply.github.com>
2023-11-08 09:10:38 +0100

    adding objects to and contructors to logic classes

--- Application/Logic/FieldLogic.cs
+++ Application/Logic/FieldLogic.cs
@@ -1,6 +1,13 @@
-﻿namespace Application.Logic;
+﻿using Application.DAOInterface;
+
+namespace Application.Logic;
 
 public class FieldLogic
 {
-    
+    private readonly IFieldDao fieldDao;
+
+    public FieldLogic(IFieldDao fieldDao)
+    {
+        this.fieldDao = fieldDao;
+    }
 }
--- Application/Logic/SensorLogic.cs
+++ Application/Logic/SensorLogic.cs
@@ -1,6 +1,13 @@
-﻿namespace Application.Logic;
+﻿using Application.DAOInterface;
+
+namespace Application.Logic;
 
 public class SensorLogic
 {
-    
+    private readonly ISensorDao sensorDao;
+
+    public SensorLogic(ISensorDao sensorDao)
+    {
+        this.sensorDao = sensorDao;
+    }
 }
