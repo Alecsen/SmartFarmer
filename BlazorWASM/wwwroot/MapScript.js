5d3ec8ee7f768dc567fc291e3d85347258b1f181
Alexander Kubel <101325014+Alecsen@users.noreply.github.com>
2023-11-16 17:45:01 +0100

    adding some map components, i dont totally understand how it is working. But right now we are able to display a map, be adding some things _Host.cshtml

---
+++ BlazorWASM/wwwroot/MapScript.js
@@ -0,0 +1,7 @@
+﻿function initializeMap() {
+    var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+}
