b1c89b51dab4c5912f9e3bfe5310ef238ac3d786
Alexander Kubel <101325014+Alecsen@users.noreply.github.com>
2023-11-17 18:31:53 +0100

    adding the first ever test for method

---
+++ Application.Test/UserLogicTests.cs
@@ -0,0 +1,38 @@
+﻿using Application.DAOInterface;
+using Application.Logic;
+using Domain.DTOs;
+using Domain.Models;
+using FluentAssertions;
+using Moq;
+
+namespace Application.Test;
+
+public class UserLogicTests
+{
+    private readonly UserLogic sut;
+    private readonly Mock<IUserDao> userDaoMock;
+
+    public UserLogicTests()
+    {
+       userDaoMock = new Mock<IUserDao>();
+       sut = new UserLogic(userDaoMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAsync_ShouldReturnUser_WhenUserExists()
+    {
+        // Arrange
+        var userName = "alecsen";
+        var UserSearchParameters = new UserSearchParametersDTO(userName);
+        var expectedUser = new AuthenticationUser { Username = userName };
+
+        userDaoMock.Setup(dao => dao.GetByUsernameAsync(userName)).ReturnsAsync(expectedUser);
+        
+        // Act
+        var user = await sut.GetAsync(UserSearchParameters);
+
+        // Assert
+        user.Should().NotBeNull();
+        Assert.Equal(userName, user.Username);
+    }
+}
