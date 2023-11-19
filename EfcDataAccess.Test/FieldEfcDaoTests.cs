a7f93b189471e8f44770415dfc4f9fb71af7b3e7
Alexander Kubel <101325014+Alecsen@users.noreply.github.com>
2023-11-19 15:11:43 +0100

    Adding a test folder for furture unit tests, i have been strugling with getting to test on the EFCDAO layer.

---
+++ EfcDataAccess.Test/FieldEfcDaoTests.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Models;
+using EfcDataAccess.DAOs;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace EfcDataAccess.Test;
+
+public class FieldEfcDaoTests
+{
+    private readonly Mock<SmartFarmerAppContext> mockContext;
+    private readonly FieldEfcDao sut;
+
+    public FieldEfcDaoTests()
+    {
+        mockContext = new Mock<SmartFarmerAppContext>();
+        sut = new FieldEfcDao(mockContext.Object);
+    }
+
+    /*
+[Fact]
+    public async Task GetFieldsByOwnerId_ReturnAListOfFields_WhenAllCriteriasAreMeet()
+    {
+        //Arrange 
+        var ownerId = 1;
+        var fields = new List<Field>
+        {
+            new Field { Id = 1, OwnerId = ownerId, Name = "field1", LocationData = "location" },
+            new Field { Id = 2, OwnerId = ownerId, Name = "field2", LocationData = "location" }
+        };
+        var mockDbSet = new Mock<DbSet<Field>>();
+        mockDbSet.As<IQueryable<Field>>().Setup(m => m.Provider).Returns(fields.AsQueryable().Provider);
+        mockDbSet.As<IQueryable<Field>>().Setup(m => m.Expression).Returns(fields.AsQueryable().Expression);
+        mockDbSet.As<IQueryable<Field>>().Setup(m => m.ElementType).Returns(fields.AsQueryable().ElementType);
+        mockDbSet.As<IQueryable<Field>>().Setup(m => m.GetEnumerator()).Returns(fields.GetEnumerator());
+        mockContext.Setup(c => c.Fields).Returns(mockDbSet.Object);
+        
+        var result = await sut.GetFieldsByOwnerId(ownerId);
+        
+        Assert.NotNull(result);
+        Assert.Equal(fields.Count, result.Count());
+    }
+
+    */
+    
+}
