c6d397c8b52b91f0f656273711b21bfeb0c7eceb
Alexander Kubel <101325014+Alecsen@users.noreply.github.com>
2023-11-20 18:13:47 +0100

    Deletig some comments

--- SmartFarmer.IntegrationTest/FieldsEndpointTests.cs
+++ SmartFarmer.IntegrationTest/FieldsEndpointTests.cs
@@ -14,33 +14,33 @@ public class FieldEndpointTests : IClassFixture<CustomWebApplicationFactory>
     public FieldEndpointTests(CustomWebApplicationFactory factory, ITestOutputHelper testOutputHelper)
     {
         client = factory.CreateClient();
-        factory.SeedDatabase(); // Seed databasen én gang for alle tests i denne klasse
+        factory.SeedDatabase(); 
     }
 
     [Fact]
     public async Task Get_FieldsByOwner_ReturnsFields()
     {
         // Arrange
-        var ownerId = 1; // Erstat med en gyldig ejer ID
+        var ownerId = 1; 
         var url = $"/Field/{ownerId}";
 
         // Act
         var response = await client.GetAsync(url);
 
         // Assert
-        response.EnsureSuccessStatusCode(); // Dette vil fejle, hvis statuskoden ikke er 200-serien
+        response.EnsureSuccessStatusCode(); 
         var responseString = await response.Content.ReadAsStringAsync();
         var fields = JsonConvert.DeserializeObject<IEnumerable<FieldLookupDto>>(responseString);
 
-        Assert.NotNull(fields); // Sikrer, at vi faktisk fik nogle data tilbage
-        Assert.NotEmpty(fields); // Sikrer, at ejeren har mindst ét felt
+        Assert.NotNull(fields); 
+        Assert.NotEmpty(fields); 
     }
 
     [Fact]
     public async Task Get_FieldByOwner_DoesNotReturnFieldWhenWrongOwnerId()
     {
         // Arrange
-        var ownerId = 1000; // Erstat med en ugyldig ejer ID
+        var ownerId = 1000; 
         var url = $"/Field/{ownerId}";
 
         // Act
