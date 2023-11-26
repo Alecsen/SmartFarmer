353878b0329a9d88c955a78de10bde59806877d1
Alexander Kubel <101325014+Alecsen@users.noreply.github.com>
2023-11-26 11:45:09 +0100

    Add sensor logic and dataaccess, along with a function in FieldEfcDao.cs to get a Field by the id

--- Application/Logic/SensorLogic.cs
+++ Application/Logic/SensorLogic.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Security.Cryptography.X509Certificates;
 using Application.DAOInterface;
 using Application.LogicInterface;
 using Domain.DTOs;
@@ -9,7 +10,7 @@ namespace Application.Logic;
 public class SensorLogic : ISensorLogic
 {
     private readonly ISensorDao sensorDao;
-
+    private readonly IFieldDao fieldDao;
     public SensorLogic(ISensorDao sensorDao)
     {
         this.sensorDao = sensorDao;
@@ -20,8 +21,18 @@ public class SensorLogic : ISensorLogic
         return sensorDao.GetSensorBySensorId(fieldId);
     }
 
-    public Task<Sensor> CreateSensorAsync(SensorCreationDto dto)
+    public async Task<Sensor> CreateSensorAsync(SensorCreationDto dto)
     {
-        
+        var field = await fieldDao.GetFieldById(dto.FieldId);
+        Random random = new Random();
+        Sensor sensor = new Sensor
+        {
+            FieldId = field.Id,
+            Latitude = dto.Latitude,
+            Longitude = dto.Longitude,
+            MoistureLevel = random.Next(0, 100)
+        };
+
+        return await sensorDao.CreateSensorAsync(sensor);
     }
 }
