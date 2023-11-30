c95c361dd67bedacde60985305d135a67736c8a8
MariaSkovsgaard <maria_skovsgaard@hotmail.com>
2023-11-30 12:59:27 +0100

    added vandingsanlæg ish

---
+++ Application/LogicInterface/IIrrigationMachineLogic.cs
@@ -0,0 +1,11 @@
+﻿using Domain.DTOs;
+using Domain.Models;
+
+namespace Application.LogicInterface;
+
+public interface IIrrigationMachineLogic
+{
+    public Task<IEnumerable<IrrigationMachineLookupDto>> GetAsync(int ownerId);
+
+    Task<IrrigationMachine> CreateAsync(IrrigationMachineCreationDto dto);
+}
---
+++ WepAPI/Controllers/IrrigationMachineController.cs
@@ -0,0 +1,51 @@
+﻿using Application.LogicInterface;
+using Domain.DTOs;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WepAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class IrrigationMachineController: ControllerBase
+{
+    private readonly IIrrigationMachineLogic irrigationMachineLogic;
+
+    public IrrigationMachineController(IIrrigationMachineLogic irrigationMachineLogic)
+    {
+        this.irrigationMachineLogic = irrigationMachineLogic;
+    }
+    
+    [HttpGet("{fieldId}")]
+    public async Task<ActionResult<IEnumerable<IrrigationMachineLookupDto>>> GetFieldsById(int fieldId)
+    {
+        try
+        {
+            var fields = await irrigationMachineLogic.GetAsync(fieldId);
+            return Ok(fields);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<IrrigationMachine>> CreateAsync([FromBody] IrrigationMachineCreationDto dto)
+    {
+        try
+        {
+            IrrigationMachine created = await irrigationMachineLogic.CreateAsync(dto);
+            return Created($"/irrigationMachine/{created.Id}", created);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+}
+    
+    
